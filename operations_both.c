fb3fce47b846f590ea30378f5e2cb50435376632
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-06-06 16:25:48 +0200

    Update operations_both.c

--- operations_both.c
+++ operations_both.c
@@ -31,3 +31,51 @@ void	ss(t_stack **stack_a, t_stack **stack_b)
 	(*stack_b)->next = tmp;
 	write(1, "ss\n", 3);
 }
+
+// Second part of the rrr function
+void	rrr_sub(t_stack **b)
+{
+	t_stack	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = *b;
+	while ((*b)->next)
+	{
+		i++;
+		*b = (*b)->next;
+	}
+	(*b)->next = tmp;
+	while (i > 1)
+	{
+		tmp = tmp->next;
+		i--;
+	}
+	tmp->next = NULL;
+	write(1, "rrr\n", 4);
+}
+
+// rrr : rra and rrb at the same time.
+void	rrr(t_stack **a, t_stack **b)
+{
+	t_stack	*tmp;
+	int		i;
+
+	if (!*a || !((*a)->next) || !*b || !((*b)->next))
+		return ;
+	i = 0;
+	tmp = *a;
+	while ((*a)->next)
+	{
+		i++;
+		*a = (*a)->next;
+	}
+	(*a)->next = tmp;
+	while (i > 1)
+	{
+		tmp = tmp->next;
+		i--;
+	}
+	tmp->next = NULL;
+	rrr_sub(b);
+}
