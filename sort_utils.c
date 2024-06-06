11f78f2373725b95363f3a2a78e3c38a9d160e69
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-06-06 16:26:43 +0200

    Update sort_utils.c

--- sort_utils.c
+++ sort_utils.c
@@ -1,35 +1,64 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_utils.c                                       :+:      :+:    :+:   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 14:18:54 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/05/27 16:13:29 by dslaveev         ###   ########.fr       */
+/*   Created: 2024/05/27 15:20:32 by dslaveev          #+#    #+#             */
+/*   Updated: 2024/05/27 15:20:54 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_sort(t_stack **stack_one, t_stack **stack_two)
+t_stack	*find_min(t_stack *stack)
 {
-	if (if_sorted(*stack_one) == 0)
-		return ;
-	if (if_sorted(*stack_one) != 0)
-		sort(stack_one, stack_two);
-}
+	t_stack	*min_node;
+	t_stack	*cur;
 
-int	if_sorted(t_stack *stack_one)
-{
-	t_stack	*tmp;
-
-	tmp = stack_one;
-	while (tmp->next)
+	if (stack == NULL)
+		return (NULL);
+	min_node = stack;
+	cur = stack->next;
+	while (cur != NULL)
 	{
-		if (tmp->nbr > tmp->next->nbr)
-			return (1);
-		tmp = tmp->next;
+		if (cur->nbr < min_node->nbr)
+			min_node = cur;
+		cur = cur->next;
 	}
-	return (0);
+	return (min_node);
 }
+
+t_stack	*biggest_node(t_stack *stack)
+{
+	t_stack	*big_node;
+	t_stack	*cur;
+
+	if (stack == NULL)
+		return (NULL);
+	big_node = stack;
+	cur = stack->next;
+	while (cur != NULL)
+	{
+		if (cur->nbr > big_node->nbr)
+			big_node = cur;
+		cur = cur->next;
+	}
+	return (big_node);
+}
+
+// int	ft_stack_len(t_stack *stack)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	if (stack == NULL)
+// 		return (0);
+// 	while (stack)
+// 	{
+// 		i++;
+// 		stack = stack->next;
+// 	}
+// 	return (i);
+// }
