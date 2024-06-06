8592a73e692f1887464c168009574e972a0417a8
Dimitar Slaveev <114510055+Slaveev@users.noreply.github.com>
2024-06-06 16:27:20 +0200

    Update sort_utils.c

--- sort_utils.c
+++ sort_utils.c
@@ -1,64 +1,78 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*   sort_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dslaveev <dslaveev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 15:20:32 by dslaveev          #+#    #+#             */
-/*   Updated: 2024/05/27 15:20:54 by dslaveev         ###   ########.fr       */
+/*   Created: 2024/05/27 14:18:54 by dslaveev          #+#    #+#             */
+/*   Updated: 2024/05/27 16:13:29 by dslaveev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*find_min(t_stack *stack)
+void	check_sort(t_stack **stack_one, t_stack **stack_two)
 {
-	t_stack	*min_node;
-	t_stack	*cur;
-
-	if (stack == NULL)
-		return (NULL);
-	min_node = stack;
-	cur = stack->next;
-	while (cur != NULL)
-	{
-		if (cur->nbr < min_node->nbr)
-			min_node = cur;
-		cur = cur->next;
-	}
-	return (min_node);
+	if (if_sorted(*stack_one) == 0)
+		return ;
+	if (if_sorted(*stack_one) != 0)
+		sort(stack_one, stack_two);
 }
 
-t_stack	*biggest_node(t_stack *stack)
+int	if_sorted(t_stack *stack_one)
 {
-	t_stack	*big_node;
-	t_stack	*cur;
+	t_stack	*tmp;
 
-	if (stack == NULL)
-		return (NULL);
-	big_node = stack;
-	cur = stack->next;
-	while (cur != NULL)
+	tmp = stack_one;
+	while (tmp->next)
 	{
-		if (cur->nbr > big_node->nbr)
-			big_node = cur;
-		cur = cur->next;
+		if (tmp->nbr > tmp->next->nbr)
+			return (1);
+		tmp = tmp->next;
 	}
-	return (big_node);
+	return (0);
 }
 
-// int	ft_stack_len(t_stack *stack)
+int	find_median(t_stack *stack, int size)
+{
+	int *values = malloc(sizeof(int) * size);
+	t_stack *current = stack;
+	int i = 0;
+
+	while (current)
+	{
+		values[i++] = current->nbr;
+		current = current->next;
+	}
+	qsort(values, size, sizeof(int), compare_int);
+	int median = values[size / 2];
+	free(values);
+	return (median);
+}
+
+int compare_int(const void *a, const void *b)
+{
+	return (*(int *)a - *(int *)b);
+}
+
+// void partition(t_stack **a, t_stack **b, int median)
 // {
-// 	int	i;
+// 	int size = ft_stack_len(*a);
+// 	int pushes = 0;
+// 	int i = 0;
 
-// 	i = 0;
-// 	if (stack == NULL)
-// 		return (0);
-// 	while (stack)
+// 	while (i < size)
 // 	{
+// 		if ((*a)->nbr <= median)
+// 		{
+// 			pb(a, b);
+// 			pushes++;
+// 		}
+// 		else
+// 		{
+// 			ra(a);
+// 		}
 // 		i++;
-// 		stack = stack->next;
 // 	}
-// 	return (i);
 // }
